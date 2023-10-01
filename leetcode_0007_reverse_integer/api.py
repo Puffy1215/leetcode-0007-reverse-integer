bb884bbb85d8da4933d083dff084065d25f67073
Andy Xie <andy.xie.opensource@gmail.com>
2023-10-01 16:46:13 +0800

    api.py: specify arg

--- leetcode_0007_reverse_integer/api.py
+++ leetcode_0007_reverse_integer/api.py
@@ -1,12 +1,13 @@
 """API for solving problem Reverse Integer"""
 
-def _check_preconditions(...) -> bool:
+
+def _check_preconditions(x: int) -> bool:
     pass
 
 
-def reverse_integer(...) -> ...:
+def reverse_integer(x: int) -> int:
     """Solves problem Reverse Integer"""
 
-    assert _check_preconditions(...)
+    assert _check_preconditions(x)
 
     pass
