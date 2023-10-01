00c601f652a0785e12a9e659ef739b64dd065a7c
Andy Xie <andy.xie.opensource@gmail.com>
2023-10-01 21:35:34 +0800

    Define math operations

--- leetcode_0007_reverse_integer/api.py
+++ leetcode_0007_reverse_integer/api.py
@@ -1,5 +1,7 @@
 """API for solving problem Reverse Integer"""
 
+from math import ceil, floor
+
 X_MAX = 2**31 - 1
 X_MIN = -(2**31)
 
@@ -8,17 +10,36 @@ def _check_preconditions(x: int) -> bool:
     return X_MIN <= x <= X_MAX
 
 
+def _posdiv(x: int) -> int:
+    assert x >= 0
+    return floor(x / 10)
+
+
+def _negdiv(x: int) -> int:
+    assert x <= 0
+    return ceil(x / 10)
+
+
+def _posmod(x: int) -> int:
+    assert x >= 0
+    return x % 10
+
+
+def _negmod(x: int) -> int:
+    assert x <= 0
+    return (x % 10) - 10
+
+
 def reverse_integer(x: int) -> int:
     """Solves problem Reverse Integer"""
 
     assert _check_preconditions(x)
 
-    sign = 1 if x == abs(x) else -1
-    x = abs(x)
+    div, mod = (_posdiv, _posmod) if x > 0 else (_negdiv, _negmod)
 
     y = 0
-    while x > 0:
-        y = y * 10 + (x % 10)
-        x = x // 10
+    while x != 0:
+        y = y * 10 + mod(x)
+        x = div(x)
 
-    return sign * y
+    return y
