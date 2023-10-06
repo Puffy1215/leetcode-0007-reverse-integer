d67480aa885aacbd1f349ae56f4e8083c5adebb4
Andy Xie <andy.xie.opensource@gmail.com>
2023-10-06 20:42:58 +0800

    test_api.py: Add rand generation fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,8 @@
 """Tests API for solving problem Reverse Integer"""
 
+import random
+from typing import Callable
+
 import pytest
 
 from leetcode_0007_reverse_integer import api
@@ -19,3 +22,13 @@ def test_reverse_integer(result: int, x: int) -> None:
     """Tests solution for problem Reverse Integer"""
 
     assert api.reverse_integer(x) == result
+
+
+@pytest.fixture
+def x_rand() -> Callable[[], int]:
+    """Fixture to generate random x"""
+
+    def _x_rand() -> int:
+        return random.randint(api.X_MIN, api.X_MAX)
+
+    return _x_rand
