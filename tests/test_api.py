e10e5a04ab0f9820e69a7e9ed4dc2a75529a51f7
Andy Xie <andy.xie.opensource@gmail.com>
2023-10-07 20:15:03 +0800

    test_api.py: Add test_reverse_integer_rand()

--- tests/test_api.py
+++ tests/test_api.py
@@ -33,3 +33,21 @@ def x_rand() -> Callable[[], int]:
         return random.randint(api.X_MIN, api.X_MAX)
 
     return _x_rand
+
+
+@pytest.mark.parametrize("run_count", range(10))
+def test_reverse_integer_rand(
+    run_count: int,
+    x_rand: Callable[[], int],  # pylint: disable=redefined-outer-name
+) -> None:
+    """Tests solution for problem Reverse Integer with random x"""
+
+    random.seed(run_count)
+
+    x = x_rand()
+    sign = 1 if x >= 0 else -1
+    result = sign * int(str(abs(x))[::-1])
+    if result < api.X_MIN or api.X_MAX < result:
+        result = 0
+
+    test_reverse_integer(result, x)
